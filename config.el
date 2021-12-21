31f105fa8f915317e65eb770c122a8029780a519
Abel Tran <abeltran.develop@gmail.com>
2021-12-21 12:46:14 +0700

    Udate init.el config

--- config.el
+++ config.el
@@ -20,7 +20,35 @@
       mouse-wheel-scroll-amount '(1 ((shift) . 1))
       )
 
-(map!
- "s-k"          #'move-text-up
- "s-j"          #'move-text-down
+(
+ map!
+ :leader
+ :nv    "SPC"   #'execute-extended-command
+
+ ;; m -> Treemacs
+ :n     "mt"    #'treemacs
+ :n     "mdf"           #'treemacs-delete-file
+ :n     "mdp"           #'treemacs-remove-project-from-workspace
+ :n     "mcd"           #'treemacs-create-dir
+ :n     "mcf"           #'treemacs-create-file
+ :n     "ma"           #'treemacs-add-project-to-workspace
+ :n     "mwc"           #'treemacs-create-workspace
+ :n     "mws"           #'treemacs-switch-workspace
+ :n     "mwd"           #'treemacs-remove-workspace
+ :n     "mwf"           #'treemacs-rename-workspace
+
+  ;; d -> edit
+ :n     "es"            #'sudo-edit
+
+ ;; w -> window
+ :n     "wo"            #'delete-other-windows
+
+ ;; i -> Insert, Imenu
+ :n     "ia"            #'+org/attach-file-and-insert-link
+ :n     "im"            #'imenu-list
+ :n     "iM"            #'lsp-ui-imenu
+ :n     "is"            #'lsp-ui-doc-show
+
+ ;; vterm
+ :n     "vt"            #'+vterm/toggle
  )
